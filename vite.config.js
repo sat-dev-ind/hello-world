f7cdd71635dcbba2bdeefdc39927227863485e0b
sat-dev-ind <dev.satyajeet.tripathy@gmail.com>
2025-09-28 08:43:20 +0200

    ci: deploy Vite to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/hello-world/',  // <-- repo name
   plugins: [react()],
   test: {
     globals: true,
