c6513bb391de3caa82ccbcbd61c70eb682fe836b
sat-dev-ind <dev.satyajeet.tripathy@gmail.com>
2025-09-28 08:11:31 +0200

    chore: add GH Pages workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'hello-world',  // set base to repo name for GitHub Pages
   test: {
     globals: true,
     environment: 'jsdom',
